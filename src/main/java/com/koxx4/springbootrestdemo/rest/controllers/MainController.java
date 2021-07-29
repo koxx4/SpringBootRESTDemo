3734eca8b36665a6be5571d208eff06be8301e28
koxx4 <piotr815@protonmail.com>
2021-07-29 22:42:19 +0200

    first commit

---
+++ src/main/java/com/koxx4/springbootrestdemo/rest/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.koxx4.springbootrestdemo.rest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    @GetMapping("/hello")
+    private String handleGetHelloService(){
+        return "Hello rest world!";
+    }
+
+}
